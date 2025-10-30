aacaf05ba0cf0bf6f7eedc210f20218920dd454a
root <root@router.test>
2025-10-30 04:09:12 +0300

    Add unmount iso

--- mount-iso.sh
+++ mount-iso.sh
@@ -5,7 +5,11 @@ mkdir ./iso-path
 
 mount Additional.iso ./iso-path
 
-cp ./iso-path ./iso-rw-path
+cp -r ./iso-path ./iso-rw-path
+
+umount ./iso-path
+
+rm -rf ./iso-path
 
 cd ./iso-rw-path/docker/ && docker load -i mariadb_latest.tar && docker load -i site_latest.tar
 
